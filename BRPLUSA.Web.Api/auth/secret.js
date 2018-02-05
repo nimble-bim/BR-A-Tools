d2634a8b6005e083be4a07b20136243d3cdf62fa
Preston Smith (Work) <PrestonII@users.noreply.github.com>
2018-02-05 11:48:17 -0500

    updated port

--- BRPLUSA.Web.Api/auth/secret.js
+++ BRPLUSA.Web.Api/auth/secret.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   env: process.env.NODE_ENV || 'dev',
-  port: process.env.POR || 7855,
+  port: process.env.PORT || 7855,
   db: {
     uri: 'mongodb://Prestonsmith2:vdDw6kHYa41j2gqv@poopypants-shard-00-00-ysi1e.mongodb.net:27017,poopypants-shard-00-01-ysi1e.mongodb.net:27017,poopypants-shard-00-02-ysi1e.mongodb.net:27017/test?ssl=true&replicaSet=PoopyPants-shard-0&authSource=admin',
     auth: "vdDw6kHYa41j2gqv"
