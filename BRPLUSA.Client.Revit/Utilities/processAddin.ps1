7af38e32fd520e09e31bd3f793e08deaeda6aeaa
Preston Smith (Work) <PrestonII@users.noreply.github.com>
2018-02-15 13:00:38 -0500

    added config based addins + script for procesing

---
+++ BRPLUSA.Client.Revit/Utilities/processAddin.ps1
@@ -0,0 +1,24 @@
+﻿function replace-file-content([string] $path, [string] $replace, [string] $replaceWith)
+{
+(Get-Content -Path $path) |
+Foreach-Object {$_ -replace $replace,$replaceWith}|
+Out-File $path
+}
+
+# get current directory
+$root = (Get-Item ".\" -Verbose).FullName
+$baseDir = [string]::Concat($root, "\Utilities")
+echo "Base Path is:  $baseDir"
+
+# find addin in the current directory
+$addin = "BRPLUSA.DEBUG.addin";
+
+# get temp variable
+$value = (Get-Item Env:\TEMP).Value
+echo "The temp folder is located here: $value"
+
+# replace %temp% with actual variable path
+$addinPath = [string]::Concat($baseDir, "\", $addin)
+$searchItem = "%TEMP%"
+echo "Replacing: $searchItem with: $value in: $addinPath"
+replace-file-content $addinPath $searchItem $value
