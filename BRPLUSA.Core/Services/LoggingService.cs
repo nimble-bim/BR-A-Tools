f7bd2b5ddd84afd99bab2f6a6d66385b76aad94a
Preston Smith (work) <PrestonII@users.noreply.github.com>
2018-10-25 11:23:13 -0400

    implemented basic logging

--- BRPLUSA.Core/Services/LoggingService.cs
+++ BRPLUSA.Core/Services/LoggingService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -8,20 +9,29 @@ namespace BRPLUSA.Core.Services
     public static class LoggingService
     {
         //private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        static LoggingService()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("newlog.log"));
+            Trace.AutoFlush = true;
+            Trace.Indent();
+        }
 
         public static void LogError(string msg, Exception innerException)
         {
             //_logger.Error(innerException, msg);
+            Trace.WriteLine(msg + innerException.Message);
+            if(innerException.InnerException != null)
+                Trace.WriteLine($"InnerException: ${innerException?.InnerException?.Message}");
         }
 
         public static void LogInfo(string msg)
         {
-            //_logger.Info(msg);
+            Trace.WriteLine(msg);
         }
 
         public static void LogWarning(string msg)
         {
-            //_logger.Warn(msg);
+            Trace.WriteLine(msg);
         }
     }
 }
