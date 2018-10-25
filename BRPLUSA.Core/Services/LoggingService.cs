8c8837461b11e626b1af12ee77f6f337dba6606f
Preston Smith (work) <PrestonII@users.noreply.github.com>
2018-10-25 10:00:38 -0400

    Remove Nlog again

--- BRPLUSA.Core/Services/LoggingService.cs
+++ BRPLUSA.Core/Services/LoggingService.cs
@@ -2,27 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NLog;
 
 namespace BRPLUSA.Core.Services
 {
     public static class LoggingService
     {
-        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        //private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
         public static void LogError(string msg, Exception innerException)
         {
-            _logger.Error(innerException, msg);
+            //_logger.Error(innerException, msg);
         }
 
         public static void LogInfo(string msg)
         {
-            _logger.Info(msg);
+            //_logger.Info(msg);
         }
 
         public static void LogWarning(string msg)
         {
-            _logger.Warn(msg);
+            //_logger.Warn(msg);
         }
     }
 }
