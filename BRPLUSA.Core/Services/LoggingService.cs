436eabd581c3295f45da6d99aa85320c6de6d565
Preston Smith (work) <PrestonII@users.noreply.github.com>
2018-10-25 14:04:26 -0400

    logging seems to be working

--- BRPLUSA.Core/Services/LoggingService.cs
+++ BRPLUSA.Core/Services/LoggingService.cs
@@ -1,50 +1,108 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using NLog.Config;
+using NLog.Targets;
+using LogFactory = NLog.LogFactory;
 
 namespace BRPLUSA.Core.Services
 {
     public static class LoggingService
     {
-        //private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private static Logger Logger { get; set; }
+
+        private static string LogDirectory { get; set; }
+
         static LoggingService()
         {
-            Trace.Listeners.Add(new TextWriterTraceListener("newlog.log"));
-            Trace.AutoFlush = true;
-            Trace.Indent();
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            SetLogDirectory();
+            var config = new LoggingConfiguration();
+
+            var error = CreateErrorLoggingTarget();
+            var full = CreateFullLoggingTarget();
+
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, error);
+            config.AddRule(LogLevel.Warn, LogLevel.Fatal, full);
+
+            LogManager.Configuration = config;
+            Logger = new LogFactory(config).GetCurrentClassLogger();
+        }
+
+        private static void SetLogDirectory()
+        {
+            var localappdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dir = $"{localappdata}/TEMP/BRPLUSA/LOGS/";
+
+            LogDirectory = dir;
+        }
+
+        private static FileTarget CreateErrorLoggingTarget()
+        {
+            var logError = new FileTarget("log_error")
+            {
+                FileName =  LogDirectory + "${shortdate}/${longdate}_error.log",
+                Layout = "${longdate}\t${level}\t${message}\t\t${exception}",
+                Header = "*********Enhancement_Log_Errors*************",
+                Encoding = Encoding.UTF8,
+                ArchiveAboveSize = 100,
+                MaxArchiveFiles = 100,
+                ArchiveFileName = "archive_${shortdate}",
+                ArchiveOldFileOnStartup = true,
+                DeleteOldFileOnStartup = false,
+                EnableFileDelete = true,
+                CreateDirs = true,
+                ConcurrentWrites = true,
+                NetworkWrites = true,
+                KeepFileOpen = true
+            };
+
+            return logError;
+        }
+
+        private static FileTarget CreateFullLoggingTarget()
+        {
+            var logFull = new FileTarget("log_full")
+            {
+                FileName = LogDirectory + "${shortdate}/${longdate}_full.log",
+                Layout = "${longdate}\t${level}\t${message}\t\t${exception}",
+                Header = "*********Enhancement_Log_Errors*************",
+                Encoding = Encoding.UTF8,
+                ArchiveAboveSize = 100,
+                MaxArchiveFiles = 100,
+                ArchiveFileName = "archive_${shortdate}",
+                ArchiveOldFileOnStartup = true,
+                DeleteOldFileOnStartup = false,
+                EnableFileDelete = true,
+                CreateDirs = true,
+                ConcurrentWrites = true,
+                NetworkWrites = true,
+                KeepFileOpen = true
+            };
+
+            return logFull;
         }
 
         public static void LogError(string msg, Exception innerException)
         {
-            //_logger.Error(innerException, msg);
-            Trace.WriteLine(msg + innerException.Message);
-            if(innerException.InnerException != null)
-                Trace.WriteLine($"InnerException: ${innerException?.InnerException?.Message}");
+            Logger.Error(innerException, msg);
         }
 
         public static void LogInfo(string msg)
         {
-            Trace.WriteLine(msg);
+            Logger.Info(msg);
         }
 
         public static void LogWarning(string msg)
         {
-            Trace.WriteLine(msg);
-        }
-    }
-
-    public class WarningLogger : TraceListener
-    {
-        public override void Write(string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void WriteLine(string message)
-        {
-            throw new NotImplementedException();
+            Logger.Warn(msg);
         }
     }
 }
