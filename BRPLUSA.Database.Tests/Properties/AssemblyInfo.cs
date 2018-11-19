a4ffc7b121b3dd49482a0f28367be5f9edfb9605
Preston (work) <PSmith@brplusa.com>
2018-11-19 15:43:57 -0500

    - added gitversion, nuget commandline & squirrel - added msbuild tasks - add batch file for autoversion updating

--- BRPLUSA.Database.Tests/Properties/AssemblyInfo.cs
+++ BRPLUSA.Database.Tests/Properties/AssemblyInfo.cs
@@ -31,6 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.1-gitversions.1+17.Branch.feature/gitversions.Sha.c3e5200725588a789761b8519f1f0b200115b890")]
