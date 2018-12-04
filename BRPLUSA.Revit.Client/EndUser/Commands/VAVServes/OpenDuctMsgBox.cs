f8e948a05252f11dd105206c59ee71a328bb886f
Preston (work) <PSmith@brplusa.com>
2018-12-04 15:13:58 -0500

    added old vavserves code in current state

---
+++ BRPLUSA.Revit.Client/EndUser/Commands/VAVServes/OpenDuctMsgBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BRPLUSA.Revit.Client.EndUser.Commands.VAVServes
+{
+    public partial class OpenDuctMsgBox : Form
+    {
+        public OpenDuctMsgBox()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
+        }
+
+        private void OpenDuctMsgBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
